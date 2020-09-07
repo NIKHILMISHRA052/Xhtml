c327108f6024bb96adfd6cc293d8d53a055a9bf4
nikhil <nikhilmishra052@gmail.com>
2020-09-07 13:22:52 +0530

    [master]

--- src/Xhtml/GetXhtmlFile.java
+++ src/Xhtml/GetXhtmlFile.java
@@ -34,46 +34,14 @@ import javax.xml.transform.stream.StreamSource;
 public class GetXhtmlFile {
 	
 	public static void main(String[] args) throws Exception {
-		//Map<String, String> topicMap = new HashMap<String, String>();
+		
 		Map<Integer, String> guidelineMap = new HashMap<Integer, String>();
 		Map<String, Integer> data = new HashMap<String, Integer>();
-		//Map<Object,ArrayList<Object>> data = new HashMap<>();
+		
 		Map<Integer, String> url = new HashMap<Integer, String>();
-		/*try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tgl_digiedit_qa", "root", "root");
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select book_id, chapter_id from `tbl_title_url`");
-			while (rs.next())
-				data.put(rs.getString(1), rs.getInt(2));
-				//data.put(rs.getObject(Int(2)), value)
-			//url.put(rs.getInt(2), rs.getString(3)); 
-			//while (rs.next())
-				//data.put(rs.getString(1), rs.getInt(2));
-			//url.put(rs.getInt(2), rs.getString(3)); 
-			
-			con.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}*/
-		/*try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tgl_digiedit_qa", "root", "root");
-			Statement stmt = con.createStatement();
-			ResultSet rst = stmt.executeQuery("select chapter_id, url from `tbl_title_url`");
-			while (rst.next())
-				url.put(rst.getInt(2),rst.getString(1) );
-				//data.put(rs.getObject(Int(2)), value)
-			//url.put(rs.getInt(2), rs.getString(3)); 
-			//while (rs.next())
-				//data.put(rs.getString(1), rs.getInt(2));
-			//url.put(rs.getInt(2), rs.getString(3)); 
-			
-			con.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}*/
-		/*guidelineMap.put(901, "Antibiotic");
+		
+
+		guidelineMap.put(901, "Antibiotic");
 		guidelineMap.put(966,"Diabetes");
 		guidelineMap.put(967,"Sexual and Reproductive Health");
 		guidelineMap.put(902,"Bone and Metabolism");
@@ -81,7 +49,6 @@ public class GetXhtmlFile {
 		guidelineMap.put(903, "Oral and Dental");
 		guidelineMap.put(981, "Other");
 		guidelineMap.put(982, "Fatigue");
-		guidelineMap.put(984, "Toxicology and Wilderness");
 		guidelineMap.put(988, "Respiratory");
 		guidelineMap.put(991, "Analgesic");
 		guidelineMap.put(993, "Psychotropic");
@@ -91,19 +58,12 @@ public class GetXhtmlFile {
 		guidelineMap.put(997, "Palliative Care");
 		guidelineMap.put(998, "Gastrointestinal");
 		guidelineMap.put(999, "Dermatology");
-		guidelineMap.put(2000, "Quicklinks");*/
+		guidelineMap.put(2000, "Quicklinks");
+		guidelineMap.put(904, "Toxicology and Toxinology");
+		guidelineMap.put(905, "Wilderness Medicine");
 
 		try {
-			//Source Xhtml = null;
 
-			//Source Xhtmlfile = new StreamSource(new File("D:\\digi content\\tgl\\digiEdit"));
-			
-
-		//	TransformerFactory tFactory = TransformerFactory.newInstance("net.sf.saxon.TransformerFactoryImpl",
-				//	XhtmlRead.class.getClassLoader());
-		//	Transformer transformer = tFactory.newTransformer(Xhtmlfile);
-
-			//File folder = new File("D:\\digi content\\tgl\\digiEdit\\");
 			File folder = new File("G:\\xhmlcontent\\");
 			File[] listOfFiles1 = folder.listFiles();
 			
@@ -113,8 +73,6 @@ public class GetXhtmlFile {
 				String pathreplace=nee+"\\";
 			    pathreplace=nee.replace("xhmlcontent", "digi content");
 			    pathreplace=pathreplace+"\\";
-				//File pathreplacedir=new File(pathreplace);
-				//String fileName = nee.substring(nee.lastIndexOf("/"));
 				String nam = new File(nee).getName();
 
 				File[] listOfFiles = listOfFiles1[j].listFiles();
@@ -124,9 +82,6 @@ public class GetXhtmlFile {
 					String na = new File(neee).getName();
 					
 					File[] listOfFiles2 = listOfFiles[i].listFiles();
-					/*for(int k = 0; k < listOfFiles2.length; k++) {
-						File[] listOfFiles3 = listOfFiles[k].listFiles(); 
-						for(int l = 0; l < listOfFiles3.length; l++){*/
 							String ne=neee+ File.separator +"digieditcontent";
 							File directory=new File(ne);
 						    int fileCount=directory.list().length;
@@ -153,81 +108,10 @@ public class GetXhtmlFile {
 					        		   rename(nam,na,filePathdirectory);
 					        	  }
 					        	  break;
-					        	  
-					        	  
-					        	 /* InputStream inStream = null;
-					        	    OutputStream outStream = null;
-
-				                 System.out.println("Directory created successfully");
-					        	  
-					        	  if(filePathdirectory.exists()){
-					        		  Path filePath1 = Paths.get(ne+"/"+namenew); 
-					        		  File afile = new File(filePath.toString());
-					        	        File bfile = new File(.toString()+"\\");
-					        		  inStream = new FileInputStream(bfile);
-					        	        outStream = new FileOutputStream(afile);
-
-					        	        byte[] buffer = new byte[1024];
-
-					        	        int length;
-					        	        //copy the file content in bytes 
-					        	        while ((length = inStream.read(buffer)) > 0) {
-					        	            outStream.write(buffer, 0, length);
-					        	        }
-
-					        	        inStream.close();
-					        	        outStream.close();  
-					            	 // Files.copy(filePath1, filePath);
-					                 System.out.println("Directory created successfully");
-					        	  }
-					        	  else{
-					        	  boolean bool = filePathdirectory.mkdir();
-					              if(bool){
-					            	  Path filePath1 = Paths.get(ne+"/"+namenew); 
-					        		  File afile = new File(filePath.toString());
-					        	        File bfile = new File(filePath1.toString());
-					        		  inStream = new FileInputStream(afile);
-					        	        outStream = new FileOutputStream(bfile);
-
-					        	        byte[] buffer = new byte[1024];
-
-					        	        int length;
-					        	        //copy the file content in bytes 
-					        	        while ((length = inStream.read(buffer)) > 0) {
-					        	            outStream.write(buffer, 0, length);
-					        	        }
-
-					        	        inStream.close();
-					        	        outStream.close();  
-					            	 // Files.copy(filePath1, filePath);
-					                 System.out.println("Directory created successfully");
-					              }else{
-					                 System.out.println("Sorry couldn’t create specified directory");
-					              }
-					        	  
-					        	
-					        	  
-					        	  
-					        	  }*/
 					        
-					          }
-					         /* else if(namenew.equals("Images")){
-					        	  File directory1=new File(ne+"/"+namenew);
-					        	 // directory1.delete();
-					        	  
-					        	  deleteDirectorys(directory1);
-
-					        	  
-					          }
-					          else{
-					        	  Path filePath = Paths.get(directory+"/"+namenew); 
-					        	  Files.delete(filePath);
-					          }*/
+					          }		        
 					          
-					        }		
-
-						
-					
+					        }			
 				
 				}
 				}
@@ -313,7 +197,7 @@ public class GetXhtmlFile {
 		    	       
 		    	        in.close();
 		    	        out.close();
-		    	       // System.out.println("File copied from " + src + " to " + dest);
+		    	       
 		    	}
 			}catch(Exception e){
 				e.printStackTrace();
@@ -341,7 +225,7 @@ public class GetXhtmlFile {
 		    		//if directory not exists, create it
 		    		if(!dest.exists()){
 		    		   dest.mkdir();
-		    		  // System.out.println("Directory copied from "    + src + "  to " + dest);
+		    		  
 		    		}
 
 		    		//list all the directory contents
@@ -375,7 +259,7 @@ public class GetXhtmlFile {
 
 		    	        in.close();
 		    	        out.close();
-		    	       // System.out.println("File copied from " + src + " to " + dest);
+		    	       
 		    	}
 			}catch(Exception e){
 				e.printStackTrace();
@@ -395,9 +279,6 @@ public class GetXhtmlFile {
 		    	
 		}
      public static void rename(String nam,String na,File filePathdirectory)throws IOException{
-    	// Map<String, Integer> data = new HashMap<String, Integer>();
- 		//Map<Object,ArrayList<Object>> data = new HashMap<>();
- 		//Map<Integer, String> url = new HashMap<Integer, String>();
     	 String  firstNamew ="null";
  		try {
  			Class.forName("com.mysql.jdbc.Driver");
@@ -432,13 +313,6 @@ public class GetXhtmlFile {
 				
 			}
 			
-			
- 				//data.put(rs.getString(1), rs.getInt(2));
- 				//data.put(rs.getObject(Int(2)), value)
- 			//url.put(rs.getInt(2), rs.getString(3)); 
- 			//while (rs.next())
- 				//data.put(rs.getString(1), rs.getInt(2));
- 			//url.put(rs.getInt(2), rs.getString(3)); 
  			
  			con.close();
  		} catch (Exception e) {
